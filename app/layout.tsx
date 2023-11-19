89fda3153c5872c9cb86f2ef6017d82b0f52f655
yeonkr <1714882@sookmyung.ac.kr>
2023-11-19 23:10:16 +0900

    feat: 4장 - 레이아웃 및 페이지 만들기

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
+  // 루트 레이아웃에 추가하는 모든 UI는 애플리케이션의 모든 페이지 에서 공유됩니다.
   children,
 }: {
   children: React.ReactNode;
